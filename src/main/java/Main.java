4eb094b0807d602007c8c6ca587e2f904d865bb0
Rutger <Rutger_van_velzen@hotmail.com>
2021-07-05 14:32:57 +0200

    removed dead code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,6 @@ import io.input.CommandlineScanner;
 import io.output.ConsolePrinter;
 
 public class Main {
-    private static int credits = 50;
-
     public static void main (String... args){
         new GameManager(new CommandlineScanner(), new ConsolePrinter(), new GameState()).startNewGame();
     }
